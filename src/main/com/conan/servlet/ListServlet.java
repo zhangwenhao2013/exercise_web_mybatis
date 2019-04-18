0715006b4783010de374658587d9e74b76be147a
zhangwenhao <zhangwenhao@iboxpay.com>
2019-04-18 13:49:05 +0800

    first commit

---
+++ src/main/com/conan/servlet/ListServlet.java
@@ -0,0 +1,21 @@
+package com.conan.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        req.getRequestDispatcher("/WEB-INF/jsp/back/list.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        doGet(req, resp);
+    }
+}
